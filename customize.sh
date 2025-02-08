c2cc00945d67c274eca46ebbde255e71d7a50628
Re*Index. (ot_inc) <32851879+reindex-ot@users.noreply.github.com>
2025-02-09 07:17:30 +0900

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,3 +2,8 @@ if [ -d /data/app/*'=='/'com.adguard.android'*'==' ]; then
 mkdir -p $MODPATH/system/app/com.adguard.android
 cp -r /data/app/*'=='/'com.adguard.android'*'=='/* $MODPATH/system/app/com.adguard.android/
 fi
+
+set_perm_recursive $MODPATH 0 0 0755 0644
+
+rm -r /data/dalvik-cache
+rm -r /cache/dalvik-cache
