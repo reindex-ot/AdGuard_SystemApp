f4a4152da7b37d66c27f91eaff0faf26d88580b5
MONE FIERA <83483662+monefiera@users.noreply.github.com>
2025-02-09 06:57:04 +0900

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,7 +4,6 @@ cp -r /data/app/*'=='/'com.adguard.android'*'=='/* $MODPATH/system/app/com.adgua
 fi
 
 set_perm_recursive $MODPATH 0 0 0755 0644
-set_perm $MODPATH 0 0 0644
 
 rm -r /data/dalvik-cache
 rm -r /cache/dalvik-cache
